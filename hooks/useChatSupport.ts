1a230ef20ae01c1de83d9a7cb0da16b946455eb9
warlock <fecypher@gmail.com>
2024-07-18 22:08:11 +0100

    added

--- hooks/useChatSupport.ts
+++ hooks/useChatSupport.ts
@@ -2,17 +2,11 @@ import { useEffect } from "react";
 
 export const useChatSupport = () => {
   useEffect(() => {
-    var Tawk_API: any = Tawk_API || {},
-      Tawk_LoadStart = new Date();
-    (function () {
-      var s1: any = document.createElement("script"),
-        s0: any = document.getElementsByTagName("script")[0];
-      s1.async = true;
-      s1.src = "https://embed.tawk.to/65fe07bea0c6737bd123c79b/1hpk4sgi7";
-      s1.charset = "UTF-8";
-      s1.setAttribute("crossorigin", "*");
-      s0.parentNode.insertBefore(s1, s0);
-    })();
+    const script = document.createElement("script");
+    script.async = true;
+    script.src = "//code.jivosite.com/widget/kLb2BvxvQ5";
+
+    document.body.appendChild(script);
   }, []);
 };
 
